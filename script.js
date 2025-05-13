b94ea5ec7a296d9865352d3785bbd276d6089c62
Igor Gonçalves Bernardo <90935717+Anacromatico@users.noreply.github.com>
2025-05-13 17:16:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+      hljs.highlightAll();
+    });
 function validar_cep(input) {
     let value = input.value.replace(/\D/g, ''); // Remove caracteres não numéricos
     if (value.length >= 8) {
